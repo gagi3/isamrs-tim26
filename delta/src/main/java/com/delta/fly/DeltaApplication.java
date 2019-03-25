d1aa36cf14ab1a91b59ba0d9098a18d192d69af0
Vladimir Gajčin <djvlada03@gmail.com>
2019-03-25 18:21:48 +0100

    Spring starter project

---
+++ delta/src/main/java/com/delta/fly/DeltaApplication.java
@@ -0,0 +1,13 @@
+package com.delta.fly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaApplication.class, args);
+	}
+
+}
---
+++ delta/src/test/java/com/delta/fly/DeltaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.delta.fly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeltaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
