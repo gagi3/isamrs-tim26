5da2dc3c4c5a8a1470f092c4d34fb6b408ac7396
Vladimir Gajčin <djvlada03@gmail.com>
2019-04-22 14:10:22 +0200

    CI

--- delta/src/main/java/com/delta/fly/security/SecurityConfiguration.java
+++ delta/src/main/java/com/delta/fly/security/SecurityConfiguration.java
@@ -19,7 +19,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
